4d03dc881a86e6e000be1ccdb467fcdb86ea5157
hjy112358 <1240661456@qq.com>
2020-08-27 17:10:59 +0800

    hjy11

--- pages/poker/pokerIndex.vue
+++ pages/poker/pokerIndex.vue
@@ -214,7 +214,7 @@
 			window.addEventListener('resize', this.handleResize)
 		},
 		methods: {
-			// 提示
+			// 提示   
 			testhit(num){
 				// 3,4,5,6,7,8,9,10, J , Q , K , A , 2 ,小王,大王
 				// 3,4,5,6,7,8,9,10, 11, 12, 13, 14, 15, 16 ,17
