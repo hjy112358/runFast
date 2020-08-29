2be5e0d2f1ee382b727f14eebdf557ee30bbe47d
hjy112358 <1240661456@qq.com>
2020-08-29 09:14:34 +0800

    串

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ import App from './App'
 import judgeCards from '@/util/judgeCards.js'
 import hintCards from './util/hintCards.js'
 // import analyzeCards from '@/util/analyzeCards.js'
+import follow from './util/follow.js'
 
 Vue.config.productionTip = false
 Vue.prototype.judgeCards = judgeCards
 // Vue.prototype.analyzeCards = analyzeCards
 Vue.prototype.hintCards = hintCards
+Vue.prototype.follow = follow
 
 
 App.mpType = 'app'
