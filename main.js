d00ce7ac482d84cbd4257b53af1fdc7d6051bcf7
codergaojia <codergaojia@github.com>
2020-08-29 14:18:09 +0800

    rem布局

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ import judgeCards from '@/util/judgeCards.js'
 import hintCards from './util/hintCards.js'
 // import analyzeCards from '@/util/analyzeCards.js'
 import follow from './util/follow.js'
+import remFn from './util/rem.js'
 
 Vue.config.productionTip = false
 Vue.prototype.judgeCards = judgeCards
 // Vue.prototype.analyzeCards = analyzeCards
 Vue.prototype.hintCards = hintCards
 Vue.prototype.follow = follow
+Vue.prototype.remFn = remFn
 
 
 App.mpType = 'app'
